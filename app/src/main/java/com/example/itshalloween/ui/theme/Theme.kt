c364da27bea55468d293ad6b877795f496816fc5
nrj022 <0602smj@gmail.com>
2025-10-28 21:25:44 +0900

    design: 기본 테마, 색상, 글꼴 설정 (#1)

--- app/src/main/java/com/example/itshalloween/ui/theme/Theme.kt
+++ app/src/main/java/com/example/itshalloween/ui/theme/Theme.kt
@@ -1,51 +1,51 @@
 package com.example.itshalloween.ui.theme
 
-import android.app.Activity
-import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = White,            // 버튼, 강조 영역
+    onPrimary = SoftBlack,        // 버튼 위의 텍스트
+
+    secondary = Orange,             // 보조 강조 색
+    onSecondary = White,      // 그 위의 아이콘/텍스트
+
+    tertiary = Gray,              // 서브 포인트 색
+    onTertiary = SoftBlack,       // 그 위의 텍스트
+
+    background = SoftBlack, // 앱 전체 배경
+    onBackground = White, // 기본 텍스트 색
+
+    surface = SoftBlack,    // 카드, 다이얼로그 등 배경
+    onSurface = White,  // 그 위의 텍스트
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = SoftBlack,            // 버튼, 강조 영역
+    onPrimary = White,        // 버튼 위의 텍스트
 
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    secondary = Orange,             // 보조 강조 색
+    onSecondary = White,      // 그 위의 아이콘/텍스트
+
+    tertiary = Gray,              // 서브 포인트 색
+    onTertiary = SoftBlack,       // 그 위의 텍스트
+
+    background = White, // 앱 전체 배경
+    onBackground = SoftBlack, // 기본 텍스트 색
+
+    surface = White,    // 카드, 다이얼로그 등 배경
+    onSurface = SoftBlack,  // 그 위의 텍스트
 )
 
 @Composable
 fun ItsHalloweenTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
-
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
     }
--- app/src/main/java/com/example/itshalloween/ui/theme/Type.kt
+++ app/src/main/java/com/example/itshalloween/ui/theme/Type.kt
@@ -2,33 +2,33 @@ package com.example.itshalloween.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.itshalloween.R
 
-// Set of Material typography styles to start with
+val customFont = FontFamily(Font(R.font.irish_grover_regular))
 val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
     titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = customFont,
         fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
+        fontSize = 20.sp,
+        lineHeight = 22.sp,
         letterSpacing = 0.sp
     ),
+    bodyMedium = TextStyle(
+        fontFamily = customFont,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp,
+        lineHeight = 22.sp,
+        letterSpacing = 0.5.sp
+    ),
     labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
+        fontFamily = customFont,
+        fontWeight = FontWeight.Normal,
+        fontSize = 12.sp,
+        lineHeight = 22.sp,
         letterSpacing = 0.5.sp
     )
-    */
 )

56163c00908260f9b96e1a2fd8cf453fcea1d9d1
nrj022 <0602smj@gmail.com>
2025-11-14 14:47:40 +0900

    chore: Firebase 연동 및 웹 뷰어 설정 (#10)
    - Firebase Analytics 라이브러리 추가
    - Babylon.js를 사용한 웹 뷰어 `index.html` 파일 추가
    - Firebase Hosting 설정 파일 및 GitHub Actions 워크플로우 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.google.services)
 }
 
 android {
@@ -41,6 +42,10 @@ android {
 }
 
 dependencies {
+    // Firebase
+    implementation(platform(libs.google.firebase.bom))
+    implementation(libs.google.firebase.analytics)
+
     implementation(project(":unityLibrary"))
     implementation(project(":unityLibrary:mobilenotifications.androidlib"))
 

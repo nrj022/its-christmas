19fa7b8b9597294552655eaf5c508f3c876edea1
nrj022 <0602smj@gmail.com>
2025-11-17 15:42:24 +0900

    feat: DB 테스트 코드 추가 및 Asset DAO 메서드 추가 (#12)
    - `inMemoryDatabase`를 사용한 `AssetEntity` 관련 DB 테스트 코드 추가
    - 테스트 코드 작성을 위해 `Truth` 의존성 추가
    - `AssetDao`에 단일 `Asset`을 삽입하는 `insert` 메서드와 모든 `Asset`을 조회하는 `getAll` 메서드 추가

--- core/database/src/main/java/com/example/database/dao/AssetDao.kt
+++ core/database/src/main/java/com/example/database/dao/AssetDao.kt
@@ -3,10 +3,17 @@ package com.example.database.dao
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
+import androidx.room.Query
 import com.example.database.entity.AssetEntity
 
 @Dao
 interface AssetDao {
     @Insert(onConflict = OnConflictStrategy.IGNORE)
-    suspend fun insertAll(assets: List<AssetEntity>)
+    suspend fun insertAll(assets: List<AssetEntity>): List<Long>
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(asset: AssetEntity): Long
+
+    @Query("SELECT * FROM assets")
+    suspend fun getAll(): List<AssetEntity>
 }

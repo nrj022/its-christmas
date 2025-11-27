de012c589dfa38597c538d137cca31be4b48f1b6
nrj022 <0602smj@gmail.com>
2025-11-27 17:16:56 +0900

    refactor: Repository에서 IO Dispatcher 사용하도록 변경 (#22)
    - 각 Repository 구현체에서 `withContext(Dispatchers.IO)`를 사용, DB 접근이 IO 스레드에서 실행되도록 보장
    - `CardElementRepositoryImpl`의 `getObjectElementsByCardId` 함수는 `flowOn(Dispatchers.IO)` 사용하도록 수정
    - `CardEditorViewModel`에서 DB 작업 호출시 명시적 `Dispatchers.IO` 코드 제거

--- core/data/src/main/java/com/example/data/repositoryImpl/CardElementRepositoryImpl.kt
+++ core/data/src/main/java/com/example/data/repositoryImpl/CardElementRepositoryImpl.kt
@@ -5,30 +5,43 @@ import com.example.data.mapper.toEntity
 import com.example.database.dao.CardElementDao
 import com.example.domain.model.CardElement
 import com.example.domain.repository.CardElementRepository
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class CardElementRepositoryImpl @Inject constructor(
     private val cardElementDao: CardElementDao
 ): CardElementRepository {
     override suspend fun insertCardElements(cardElements: List<CardElement>) {
-        cardElementDao.insertAll(cardElements.map { it.toEntity() })
+        withContext(Dispatchers.IO) {
+            cardElementDao.insertAll(cardElements.map { it.toEntity() })
+        }
     }
 
     override suspend fun insertCardElement(cardElement: CardElement): Long {
-        return cardElementDao.insertElement(cardElement.toEntity())
+        return withContext(Dispatchers.IO) {
+            cardElementDao.insertElement(cardElement.toEntity())
+        }
     }
 
     override suspend fun deleteCardElementsByCardId(cardId: Long) {
-        cardElementDao.deleteByCardId(cardId)
+        withContext(Dispatchers.IO) {
+            cardElementDao.deleteByCardId(cardId)
+        }
     }
 
     override fun getObjectElementsByCardId(cardId: Long): Flow<List<CardElement>> {
-        return cardElementDao.getObjectElementsByCardId(cardId).map { entityList -> entityList.map { it.toDomain() } }
+        return cardElementDao.getObjectElementsByCardId(cardId)
+            .map { entityList -> entityList.map { it.toDomain() } }
+            .flowOn(Dispatchers.IO)
     }
 
     override suspend fun getTextElementsByCardId(cardId: Long): List<CardElement> {
-        return cardElementDao.getTextElementsByCardId(cardId).map { it.toDomain() }
+        return withContext(Dispatchers.IO) {
+            cardElementDao.getTextElementsByCardId(cardId).map { it.toDomain() }
+        }
     }
 }

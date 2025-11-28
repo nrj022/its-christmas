e06b7fbbc04d860fd6127f81e06c523e21b3c46c
nrj022 <0602smj@gmail.com>
2025-11-28 19:50:41 +0900

    fix: 잘못된 패키지명 수정 (#25)
    - `ExampleInstrumentedTest`에서 중복된 패키지명을 수정
    - `activity_card_editor.xml`의 `tools:context` 경로 수정

--- app/src/androidTest/java/com/itschristmas/android/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/itschristmas/android/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.itschristmas.com.itschristmas.android", appContext.packageName)
+        assertEquals("com.itschristmas.android", appContext.packageName)
     }
 }

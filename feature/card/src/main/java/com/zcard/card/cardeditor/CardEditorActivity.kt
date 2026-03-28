2d00b5541ba213e1c658eea3fc45a9f51a69e781
nrj022 <0602smj@gmail.com>
2026-03-28 11:22:11 +0900

    refactor: CardEditorActivity 내 Unity onTrimMemory 로직 최적화 (#118)
    - 심각도 기반 범위 비교(>=)를 도입하여 TRIM_MEMORY_BACKGROUND 이상의 위급 상황에 대응 (MemoryUsage.High 전달)

--- feature/card/src/main/java/com/zcard/card/cardeditor/CardEditorActivity.kt
+++ feature/card/src/main/java/com/zcard/card/cardeditor/CardEditorActivity.kt
@@ -296,11 +296,11 @@ class CardEditorActivity : AppCompatActivity() {
     // Trim Memory Unity
     override fun onTrimMemory(level: Int) {
         super.onTrimMemory(level)
-        when(level) {
-            // 앱 백그라운드 이동 시
-            TRIM_MEMORY_UI_HIDDEN -> unityPlayer.onTrimMemory(UnityPlayerForActivityOrService.MemoryUsage.Medium)
+        when {
             // 앱 백그라운드 이동 + 메모리 부족 시
-            TRIM_MEMORY_BACKGROUND -> unityPlayer.onTrimMemory(UnityPlayerForActivityOrService.MemoryUsage.High)
+            level >= TRIM_MEMORY_BACKGROUND -> unityPlayer.onTrimMemory(UnityPlayerForActivityOrService.MemoryUsage.High)
+            // 앱 백그라운드 이동 시
+            level == TRIM_MEMORY_UI_HIDDEN -> unityPlayer.onTrimMemory(UnityPlayerForActivityOrService.MemoryUsage.Medium)
             else -> unityPlayer.onTrimMemory(UnityPlayerForActivityOrService.MemoryUsage.Medium)
         }
     }

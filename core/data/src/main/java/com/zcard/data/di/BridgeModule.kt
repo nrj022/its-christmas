5b83a9ad847b3331e73592c1b0cf6b98b0a41c81
nrj022 <0602smj@gmail.com>
2026-05-06 23:02:44 +0900

    refactor: DI 모듈 바인딩에 Singleton 스코프 적용 (#130)
    - RepositoryModule, StorageModule, BridgeModule 각 바인딩에 `@Singleton`을 추가하여 앱 생명주기 동안 단일 인스턴스 보장

--- core/data/src/main/java/com/zcard/data/di/BridgeModule.kt
+++ core/data/src/main/java/com/zcard/data/di/BridgeModule.kt
@@ -6,12 +6,14 @@ import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 abstract class BridgeModule {
 
     @Binds
+    @Singleton
     abstract fun bindUnityBridge(
         unityBridgeImpl: UnityBridgeImpl
     ): UnityBridge

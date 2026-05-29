0cd1c77cee78dbae053a95199fd5c6b03c945a00
nrj022 <0602smj@gmail.com>
2026-05-29 23:51:55 +0900

    refactor: CardInfoDialog 내 링크 클릭 영역 확장 (#158)
    - Row 레이아웃에 `fillMaxWidth()`를 추가하여 클릭 가능한 영역을 가로 전체로 확장

--- feature/src/main/java/com/zcard/feature/cardeditor/dialog/CardInfoDialog.kt
+++ feature/src/main/java/com/zcard/feature/cardeditor/dialog/CardInfoDialog.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
@@ -87,6 +88,7 @@ fun CardInfoDialog(
             }
             Row(
                 modifier = Modifier
+                    .fillMaxWidth()
                     .padding(top = 10.dp)
                     .clickable { onViewLink() }
                     .padding(10.dp),

9bcf85cd3b752154c6827fa351253f2d16793288
nrj022 <0602smj@gmail.com>
2026-06-09 23:56:08 +0900

    build: 앱 기본 버전 1.8.1으로 업데이트

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,7 +18,7 @@ android {
         targetSdk = 36
         versionCode = project.findProperty("versionCode")?.toString()?.toInt() ?: 4
 
-        val baseVersion = "1.8.0"
+        val baseVersion = "1.8.1"
         versionName = project.findProperty("versionName")?.toString() ?: baseVersion
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
